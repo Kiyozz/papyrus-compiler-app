57ca4515aafb6fb08784080a317f4fcc797edf49
Kevin TURMEL <kevinturmel@gmail.com>
2019-02-22 10:17:36 +0100

    Fix

--- app/papyrus-compiler-app/src/app/preferences-keys.ts
+++
@@ -1,8 +0,0 @@
-export const OUTPUT_FOLDER = 'outputFolder';
-export const IMPORTS_FOLDER = 'importsFolder';
-export const GAME_PATH_FOLDER = 'gamePathFolder';
-export const FLAG = 'flag';
-export const SCRIPTS = 'scripts';
-export const ALWAYS_COMPILE_SCRIPTS = 'always_scripts';
-
-export const scriptStringSeparator = ':-:';
