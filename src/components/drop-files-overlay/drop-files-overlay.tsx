e9036e6c2ef9633f1ca8d23f057eed6d8de88d15
Kevin TURMEL <kevinturmel@gmail.com>
2020-05-07 12:50:32 +0200

    Hotfix drop overlay

--- src/components/drop-files-overlay/drop-files-overlay.tsx
+++ src/components/drop-files-overlay/drop-files-overlay.tsx
@@ -1,4 +1,6 @@
 import Backdrop from '@material-ui/core/Backdrop'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 import React from 'react'
 import classes from './drop-files-overlay.module.scss'
 
@@ -9,7 +11,9 @@ interface Props {
 const DropFilesOverlay: React.FC<Props> = ({ open }) => {
   return (
     <Backdrop open={open} className={classes.background}>
-      Drop files
+      <Box className={classes.box} bgcolor="primary.main" color="text.primary">
+        <Typography variant="h1" component="div">Drop files</Typography>
+      </Box>
     </Backdrop>
   )
 }
