c02cc3ff8eff01f1dfacd245113ab2c1af30fea1
Kévin TURMEL <kevinturmel@gmail.com>
2020-08-14 15:51:09 +0200

    build: better build scripts

--- scripts/build.js
+++ scripts/build.js
@@ -13,10 +13,18 @@ const args = require('yargs').argv
 
 const exec = promisify(childProcess.exec)
 
-const isSkipBuild = typeof args.skipBuild !== 'undefined'
-const isForcePackage = typeof args.package !== 'undefined'
-const isForceZip = typeof args.zip !== 'undefined'
+const isBuild = typeof args.build !== 'undefined' ? args.build === true : true
+const isPackage = typeof args.package !== 'undefined' && args.package === true
+const isZip = typeof args.zip !== 'undefined' && args.zip === true
 
+if (!isBuild && !isPackage && !isZip) {
+  console.log('Nothing to do.')
+
+  process.exit(0)
+}
+
+const SE_ZIP_NAME = 'papyrus-compiler-se'
+const LE_ZIP_NAME = 'papyrus-compiler-le'
 const PROJECT_DIR = path.join(__dirname, '..')
 const PACKAGE_DIR = path.join(PROJECT_DIR, 'out')
 const BUILD_DIR = path.join(PROJECT_DIR, 'build')
@@ -25,332 +33,234 @@ const BUILD_SE = 'build-se'
 const BUILD_LE_DIR = path.join(PROJECT_DIR, BUILD_LE)
 const BUILD_SE_DIR = path.join(PROJECT_DIR, BUILD_SE)
 
-class FileHandler {
-  /**
-   *
-   * @param {string} from
-   * @param {string} to
-   * @return {Promise<void>}
-   */
-  move(from, to) {
-    return fs.move(from, to, { overwrite: true })
-  }
+/**
+ *
+ * @param {string} filename
+ * @param {string} destination
+ * @return {Promise<unknown>}
+ */
+function zip(filename, destination) {
+  const stream = Seven.add(path.resolve(PROJECT_DIR, `${filename}.7z`), '*', {
+    $bin: zipBin.path7za,
+    fullyQualifiedPaths: true,
+    workingDir: PACKAGE_DIR,
+    $progress: true,
+    method: ['0=LZMA2'],
+    noRootDuplication: true,
+    $spawnOptions: { cwd: path.resolve(destination, 'papyrus-compiler-app-win32-x64') }
+  })
 
-  /**
-   *
-   * @param {string} from
-   * @param {string} to
-   * @return {Promise<void>}
-   */
-  copy(from, to) {
-    return fs.copy(from, to, { recursive: true })
-  }
+  return new Promise(function (resolve, reject) {
+    stream.on('end', function () {
+      resolve()
+    })
+
+    stream.on('error', function (err) {
+      reject(err)
+    })
+  })
 }
 
-class Util {
-  /**
-   *
-   * @param {number} ms
-   * @returns {Promise<void>}
-   */
-  wait(ms = 1000) {
-    return new Promise(function(resolve) {
-      setTimeout(resolve, ms)
-    })
-  }
-
-  /**
-   * Ask a question to the user
-   *
-   * @param {string} question
-   * @returns {Promise<boolean>}
-   */
-  ask(question) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
-
-    return new Promise(function(resolve) {
-      rl.question(`${question} (y/n): `, function(ans) {
-        rl.close()
-
-        if (ans === '' || ans === 'y') {
-          resolve(true)
-        } else {
-          resolve(false)
-        }
-      })
-    })
-  }
+/**
+ * @param {string} icon
+ * @param {string} folderSource
+ * @return {Promise<string | string[]> | *}
+ */
+function packageApp(icon, folderSource) {
+  return packager({
+    prune: false,
+    asar: true,
+    arch: 'x64',
+    dir: path.resolve(PROJECT_DIR, folderSource),
+    out: path.resolve(PACKAGE_DIR, folderSource),
+    icon,
+    quiet: true
+  })
 }
 
-class Packager {
-  /**
-   *
-   * @param {string} icon
-   * @param {string} projectDir
-   * @param {string} packageDir
-   */
-  constructor(icon, projectDir, packageDir) {
-    this.projectDir = projectDir
-    this.packageDir = packageDir
-    this.icon = icon
-  }
-
-  package(folderSource) {
-    return packager({
-      prune: false,
-      asar: true,
-      arch: 'x64',
-      dir: path.resolve(this.projectDir, folderSource),
-      out: path.resolve(this.packageDir, folderSource),
-      icon: this.icon,
-      quiet: true
-    })
-  }
+/**
+ *
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+function wait(ms = 1000) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, ms)
+  })
 }
 
-class ZipCreator {
-  /**
-   *
-   * @param {string} projectDir
-   * @param {string} workingDir
-   */
-  constructor(projectDir, workingDir) {
-    this.projectDir = projectDir
-    this.workingDir = workingDir
-  }
+/**
+ * Ask a question to the user
+ *
+ * @param {string} question
+ * @returns {Promise<boolean>}
+ */
+function ask(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
 
-  /**
-   *
-   * @param {string} filename
-   * @param {string} destination
-   * @returns {Promise<void>}
-   */
-  start(filename, destination) {
-    const stream = Seven.add(path.resolve(this.projectDir, `${filename}.7z`), '*', {
-      $bin: zipBin.path7za,
-      fullyQualifiedPaths: true,
-      workingDir: this.workingDir,
-      $progress: true,
-      method: ['0=LZMA2'],
-      noRootDuplication: true,
-      $spawnOptions: { cwd: path.resolve(destination, 'papyrus-compiler-app-win32-x64') }
-    })
+  return new Promise(function (resolve) {
+    rl.question(`${question} (y/n): `, function (ans) {
+      rl.close()
 
-    return new Promise(function(resolve, reject) {
-      stream.on('end', function() {
-        resolve()
-      })
-
-      stream.on('error', function(err) {
-        reject(err)
-      })
-    })
-  }
-}
-
-class Build {
-  /**
-   *
-   * @param {ZipCreator} zipCreator
-   * @param {Packager} usedPackager
-   * @param {Util} util
-   * @param {FileHandler} fileHandler
-   * @param {string} projectDir
-   * @param {string} packageDir
-   * @param {string} buildDir
-   * @param {string} buildLeDir
-   * @param {string} buildSeDir
-   * @param {string} buildLe
-   * @param {string} buildSe
-   */
-  constructor(
-    zipCreator,
-    usedPackager,
-    util,
-    fileHandler,
-    projectDir,
-    packageDir,
-    buildDir,
-    buildLeDir,
-    buildSeDir,
-    buildLe,
-    buildSe
-  ) {
-    this.zipCreator = zipCreator
-    this.packager = usedPackager
-    this.util = util
-    this.fileHandler = fileHandler
-    this.projectDir = projectDir
-    this.packageDir = packageDir
-    this.buildDir = buildDir
-    this.buildLeDir = buildLeDir
-    this.buildSeDir = buildSeDir
-    this.buildSe = buildSe
-    this.buildLe = buildLe
-  }
-
-  async start() {
-    if (!isSkipBuild) {
-      await this.clean()
-      await this.build()
-    }
-
-    const done = await this.packaging()
-
-    if (done) {
-      await this.zipping()
-    }
-  }
-
-  async clean() {
-    const s = ora({ text: 'Clean builds', spinner: Spinners.simpleDotsScrolling }).start()
-
-    try {
-      await del('./build')
-      await del('./build-le')
-      await del('./build-se')
-      await del('./out')
-      await del('./papyrus-compiler-se.7z')
-      await del('./papyrus-compiler-le.7z')
-
-      s.succeed('Builds cleaned')
-    } catch (e) {
-      s.fail(e)
-    }
-  }
-
-  async build() {
-    const s = ora({ text: 'Building main', spinner: Spinners.simpleDotsScrolling }).start()
-    const sApp = ora({ indent: 1, spinner: Spinners.dots, text: 'Build' })
-
-    await this.util.wait(400)
-
-    try {
-      s.text = 'Create Legendary Edition build'
-
-      await exec('cross-env REACT_APP_NEXUS_PATH=https://www.nexusmods.com/skyrim/mods/96339?tab=files npm run build', {
-        cwd: this.projectDir
-      })
-      await this.fileHandler.move(this.buildDir, this.buildLeDir)
-
-      s.text = 'Create Special Edition build'
-
-      await exec(
-        'cross-env REACT_APP_NEXUS_PATH=https://www.nexusmods.com/skyrimspecialedition/mods/23852?tab=files npm run build',
-        { cwd: this.projectDir }
-      )
-      await this.fileHandler.move(this.buildDir, this.buildSeDir)
-
-      s.text = 'Create Main build'
-
-      await this.util.wait(500)
-
-      s.stop()
-      sApp.start()
-
-      await exec('npm run build:electron', { cwd: this.projectDir })
-
-      sApp.text = 'Assets'
-
-      await this.fileHandler.copy(
-        path.resolve(this.projectDir, 'package.json'),
-        path.resolve(this.buildDir, 'package.json')
-      )
-
-      await this.util.wait()
-
-      await this.fileHandler.copy(path.resolve(this.projectDir, 'yarn.lock'), path.resolve(this.buildDir, 'yarn.lock'))
-
-      await this.util.wait()
-
-      sApp.text = 'Dependencies'
-
-      await exec('yarn install --prod', { cwd: this.buildDir })
-
-      await this.util.wait()
-      await del(path.join(this.buildDir, 'yarn.lock'))
-      // await del(path.join(this.buildDir, 'package.json'))
-      await this.fileHandler.copy(this.buildDir, this.buildLeDir)
-      await this.fileHandler.copy(this.buildDir, this.buildSeDir)
-
-      await del(this.buildDir)
-
-      sApp.stop()
-
-      s.start()
-      s.succeed('Builds created')
-    } catch (e) {
-      sApp.stop()
-      s.fail(e.message)
-    }
-  }
-
-  async packaging() {
-    if (!isForcePackage) {
-      const response = await this.util.ask('Package the builds?')
-
-      if (!response) {
-        return false
+      if (ans === '' || ans === 'y') {
+        resolve(true)
+      } else {
+        resolve(false)
       }
-    }
+    })
+  })
+}
 
-    const s = ora({ text: 'Building Legendary Edition package', spinner: Spinners.simpleDotsScrolling }).start()
+/**
+ * @param {string} from
+ * @param {string} to
+ * @return {Promise<void>}
+ */
+function copy(from, to) {
+  return fs.copy(from, to, { recursive: true })
+}
 
-    try {
-      await this.packager.package(this.buildLe)
+/**
+ * @param {string} from
+ * @param {string} to
+ * @return {Promise<void>}
+ */
+function move(from, to) {
+  return fs.move(from, to, { overwrite: true })
+}
 
-      s.text = 'Building Special Edition package'
+async function clean() {
+  const s = ora({ text: 'Clean builds', spinner: Spinners.simpleDotsScrolling }).start()
 
-      await this.packager.package(this.buildSe)
+  try {
+    await del(BUILD_DIR)
+    await del(BUILD_LE)
+    await del(BUILD_SE)
+    await del(PACKAGE_DIR)
+    await del(`${SE_ZIP_NAME}.7z`)
+    await del(`${LE_ZIP_NAME}.7z`)
 
-      s.succeed('Package builded')
+    s.succeed('Builds cleaned')
+  } catch (e) {
+    s.fail(e)
+  }
+}
 
-      return true
-    } catch (e) {
-      s.fail(e.message)
-    }
+async function build() {
+  const s = ora({ text: 'Building main', spinner: Spinners.simpleDotsScrolling }).start()
+  const sApp = ora({ indent: 1, spinner: Spinners.dots, text: 'Build' })
+
+  await wait(400)
+
+  try {
+    s.text = 'Create Legendary Edition build'
+
+    await exec('cross-env REACT_APP_NEXUS_PATH=https://www.nexusmods.com/skyrim/mods/96339?tab=files npm run build', {
+      cwd: PROJECT_DIR
+    })
+    await move(BUILD_DIR, BUILD_LE_DIR)
+
+    s.text = 'Create Special Edition build'
+
+    await exec(
+      'cross-env REACT_APP_NEXUS_PATH=https://www.nexusmods.com/skyrimspecialedition/mods/23852?tab=files npm run build',
+      { cwd: PROJECT_DIR }
+    )
+    await move(BUILD_DIR, BUILD_SE_DIR)
+
+    s.text = 'Create Main build'
+
+    await wait(500)
+
+    s.stop()
+    sApp.start()
+
+    await exec('npm run build:electron', { cwd: PROJECT_DIR })
+
+    sApp.text = 'Assets'
+
+    await this.fileHandler.copy(path.resolve(PROJECT_DIR, 'package.json'), path.resolve(BUILD_DIR, 'package.json'))
+
+    await wait()
+
+    await copy(path.resolve(PROJECT_DIR, 'yarn.lock'), path.resolve(BUILD_DIR, 'yarn.lock'))
+
+    await wait()
+
+    sApp.text = 'Dependencies'
+
+    await exec('yarn install --prod', { cwd: BUILD_DIR })
+
+    await wait()
+    await del(path.join(BUILD_DIR, 'yarn.lock'))
+    // await del(path.join(BUILD_DIR, 'package.json'))
+    await this.fileHandler.copy(BUILD_DIR, BUILD_LE_DIR)
+    await this.fileHandler.copy(BUILD_DIR, BUILD_SE_DIR)
+
+    await del(BUILD_DIR)
+
+    sApp.stop()
+
+    s.start()
+    s.succeed('Builds created')
+  } catch (e) {
+    sApp.stop()
+    s.fail(e.message)
+  }
+}
+
+async function packaging() {
+  const s = ora({ text: 'Building Legendary Edition package', spinner: Spinners.simpleDotsScrolling }).start()
+
+  try {
+    const icon = path.resolve(PROJECT_DIR, 'src/assets/logo/app/icons/win/icon.ico')
+
+    await packageApp(icon, BUILD_LE)
+
+    s.text = 'Building Special Edition package'
+
+    await packageApp(icon, BUILD_SE)
+
+    s.succeed('Package builded')
+
+    return true
+  } catch (e) {
+    s.fail(e.message)
+  }
+}
+
+async function zipping() {
+  const s = ora({ text: 'Building Legendary Edition zip', spinner: Spinners.simpleDotsScrolling }).start()
+
+  try {
+    await zip(LE_ZIP_NAME, path.resolve(PACKAGE_DIR, BUILD_LE))
+
+    s.text = 'Building Special Edition zip'
+
+    await zip(SE_ZIP_NAME, path.resolve(PACKAGE_DIR, BUILD_SE))
+
+    s.succeed('Zips builded')
+  } catch (e) {
+    s.fail(e.message)
+  }
+}
+
+async function start() {
+  if (isBuild) {
+    await clean()
+    await build()
   }
 
-  async zipping() {
-    if (!isForceZip) {
-      const response = await this.util.ask('Zip the packages?')
+  if (isPackage) {
+    const done = await packaging()
 
-      if (!response) {
-        return false
-      }
-    }
-
-    const s = ora({ text: 'Building Legendary Edition zip', spinner: Spinners.simpleDotsScrolling }).start()
-
-    try {
-      await this.zipCreator.start('papyrus-compiler-le', path.resolve(this.packageDir, 'build-le'))
-
-      s.text = 'Building Special Edition zip'
-
-      await this.zipCreator.start('papyrus-compiler-se', path.resolve(this.packageDir, 'build-se'))
-
-      s.succeed('Zips builded')
-    } catch (e) {
-      s.fail(e.message)
+    if (done && isZip) {
+      await zipping()
     }
   }
 }
 
-const builder = new Build(
-  new ZipCreator(PROJECT_DIR, PACKAGE_DIR),
-  new Packager(path.resolve(PROJECT_DIR, 'src/assets/logo/app/icons/win/icon.ico'), PROJECT_DIR, PACKAGE_DIR),
-  new Util(),
-  new FileHandler(),
-  PROJECT_DIR,
-  PACKAGE_DIR,
-  BUILD_DIR,
-  BUILD_LE_DIR,
-  BUILD_SE_DIR,
-  BUILD_LE,
-  BUILD_SE
-)
-
-builder.start()
+start()
