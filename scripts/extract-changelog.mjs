809b9d15dab4c1dcf634ce7d2cda3e1576d20186
Kévin TURMEL <kevinturmel@gmail.com>
2021-03-17 19:38:19 +0100

    Update extract-changelog.mjs

--- scripts/extract-changelog.mjs
+++ scripts/extract-changelog.mjs
@@ -7,35 +7,28 @@
 import { promises as fs } from 'fs'
 import path from 'path'
 
-/**
- * @return {Promise<void>}
- */
-async function extract() {
-  let version = process.argv[2]
+let version = process.argv[2]
 
-  if (!version) {
-    process.exit(0)
-  }
-
-  version = version.replace('refs/tags/', '')
-
-  const versionWithoutV = version.replace('v', '')
-  const versionHeader = '##'
-  const changelog = (await fs.readFile(path.resolve('CHANGELOG.md'))).toString(
-    'utf-8'
-  )
-
-  const regExp = new RegExp(
-    `(${versionHeader}\\s(v)?${versionWithoutV}[\\s\\S]*?[^#]{3})${versionHeader}\\s`,
-    'gi'
-  )
-  const extracted = regExp.exec(changelog)?.[1]
-
-  if (typeof extracted === 'undefined') {
-    process.exit(0)
-  }
-
-  console.log(extracted.trim())
+if (!version) {
+  process.exit(0)
 }
 
-extract()
+version = version.replace('refs/tags/', '')
+
+const versionWithoutV = version.replace('v', '')
+const versionHeader = '##'
+const changelog = (await fs.readFile(path.resolve('CHANGELOG.md'))).toString(
+  'utf-8'
+)
+
+const regExp = new RegExp(
+  `(${versionHeader}\\s(v)?${versionWithoutV}[\\s\\S]*?[^#]{3})${versionHeader}\\s`,
+  'gi'
+)
+const extracted = regExp.exec(changelog)?.[1]
+
+if (typeof extracted === 'undefined') {
+  process.exit(0)
+}
+
+console.log(extracted.trim())
