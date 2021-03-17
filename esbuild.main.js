05e8384cc9c8b6a786dfaedc3a5c91a536d18fc2
Kévin TURMEL <kevinturmel@gmail.com>
2021-03-17 18:10:54 +0100

    bump electron-esbuild from 1.1.6 to 1.2.3
    bump esbuild from 0.9.2 to 0.9.3

--- esbuild.main.js
+++ esbuild.main.js
@@ -6,33 +6,26 @@
 
 const path = require('path')
 
-/**
- * @param {Partial<import('esbuild').BuildOptions>} merge
- *
- * @return {import('esbuild').BuildOptions}
- */
-module.exports = (merge = {}) => {
-  return {
-    platform: 'node',
-    entryPoints: [path.resolve('src/main/main.ts')],
-    bundle: true,
-    target: 'node14.16.0',
-    loader: {
-      '.ts': 'ts'
-    },
-    define: {
-      'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`,
-      'process.env.ELECTRON_WEBPACK_APP_MOD_URL': `'${
-        process.env.ELECTRON_WEBPACK_APP_MOD_URL ??
-        'https://www.nexusmods.com/skyrimspecialedition/mods/23852'
-      }'`,
-      'process.env.ELECTRON_TELEMETRY_API': `'${
-        process.env.ELECTRON_TELEMETRY_API ?? ''
-      }'`,
-      'process.env.ELECTRON_TELEMETRY_API_KEY': `'${
-        process.env.ELECTRON_TELEMETRY_API_KEY ?? ''
-      }'`
-    },
-    ...merge
+/** @var {Partial<import('esbuild').BuildOptions>} */
+module.exports = {
+  platform: 'node',
+  entryPoints: [path.resolve('src/main/main.ts')],
+  bundle: true,
+  target: 'node14.16.0',
+  loader: {
+    '.ts': 'ts'
+  },
+  define: {
+    'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`,
+    'process.env.ELECTRON_WEBPACK_APP_MOD_URL': `'${
+      process.env.ELECTRON_WEBPACK_APP_MOD_URL ??
+      'https://www.nexusmods.com/skyrimspecialedition/mods/23852'
+    }'`,
+    'process.env.ELECTRON_TELEMETRY_API': `'${
+      process.env.ELECTRON_TELEMETRY_API ?? ''
+    }'`,
+    'process.env.ELECTRON_TELEMETRY_API_KEY': `'${
+      process.env.ELECTRON_TELEMETRY_API_KEY ?? ''
+    }'`
   }
 }
