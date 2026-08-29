0500624e49b16ac252184ad2410bb2db6df4ab8d
Wk <kevinturmel@gmail.com>
2026-08-29 02:52:11 +0200

    feat: point documentation URL to pca.kiyozz.com

--- src/common/env.ts
+++ src/common/env.ts
@@ -7,4 +7,4 @@ export const MOD_URL_DEFAULT =
 
 export const MOD_DOCUMENTATION_URL_DEV = 'http://localhost:3001'
 
-export const MOD_DOCUMENTATION_URL = 'https://documentation.pca-app.dev'
+export const MOD_DOCUMENTATION_URL = 'https://pca.kiyozz.com'
